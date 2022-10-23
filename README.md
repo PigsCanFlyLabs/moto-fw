daed6a72d37ee16319a3f04c481a29d00e0a174a
Holden Karau <holden@pigscanfly.ca>
2022-10-22 17:43:14 -0700

    Start working on LSM6DS3TR connection.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # moto-fw
+
 Firmware for motorcycle device
+
+## Goals
+
+Detect engine brakeing (and possibly other events) and trigger break light.
+
+Possible references:
+
+- https://www.data-in-brief.com/article/S2352-3409(19)30179-9/fulltext
+- https://stackoverflow.com/questions/27158807/arduino-accelerometer-detect-braking-deceleration-on-a-bike 
+- https://www.instructables.com/Bar-End-Brake-Light-BEBL/ (bicycle)
+- https://github.com/gautamo/Smart-Bike-Seat (bicycle)
+
+
+### Secondary goals:
+
+Fall detection?
+- https://www.sciencedirect.com/science/article/abs/pii/S0967066112002304 (fall detection)
+- https://www.data-in-brief.com/article/S2352-3409(19)30179-9/fulltext (fall detection data)
+
+Security?
+- 
+
+
+## Hardware
+
+See https://github.com/PigsCanFlyLabs/motorcycle-hardware 
+Accelerometer chip is a lsm6ds3trc.
+See https://www.st.com/resource/en/application_note/an5130-lsm6ds3trc-alwayson-3d-accelerometer-and-3d-gyroscope-stmicroelectronics.pdf
--- fw/LSM6DS3TR.py
+++ fw/LSM6DS3TR.py
@@ -1,10 +1,73 @@
+
+# Constants from https://github.com/adafruit/Adafruit_CircuitPython_LSM6DS/blob/main/adafruit_lsm6ds/__init__.py
+# See descriptions of registers https://www.st.com/resource/en/application_note/an5130-lsm6ds3trc-alwayson-3d-accelerometer-and-3d-gyroscope-stmicroelectronics.pdf
+_LSM6DS_MLC_INT1 = const(0x0D)
+_LSM6DS_WHOAMI = const(0xF)
+_LSM6DS_CTRL1_XL = const(0x10)
+_LSM6DS_CTRL2_G = const(0x11)
+_LSM6DS_CTRL3_C = const(0x12)
+_LSM6DS_CTRL8_XL = const(0x17)
+_LSM6DS_CTRL9_XL = const(0x18)
+_LSM6DS_CTRL10_C = const(0x19)
+_LSM6DS_ALL_INT_SRC = const(0x1A)
+_LSM6DS_OUT_TEMP_L = const(0x20)
+_LSM6DS_OUTX_L_G = const(0x22)
+_LSM6DS_OUTX_L_A = const(0x28)
+_LSM6DS_MLC_STATUS = const(0x38)
+_LSM6DS_STEP_COUNTER = const(0x4B)
+_LSM6DS_TAP_CFG0 = const(0x56)
+_LSM6DS_TAP_CFG = const(0x58)
+_LSM6DS_MLC0_SRC = const(0x70)
+_MILLI_G_TO_ACCEL = 0.00980665
+_TEMPERATURE_SENSITIVITY = 256
+_TEMPERATURE_OFFSET = 25.0
+
+_LSM6DS_EMB_FUNC_EN_A = const(0x04)
+_LSM6DS_EMB_FUNC_EN_B = const(0x05)
+_LSM6DS_FUNC_CFG_ACCESS = const(0x01)
+_LSM6DS_FUNC_CFG_BANK_USER = const(0)
+_LSM6DS_FUNC_CFG_BANK_HUB = const(1)
+_LSM6DS_FUNC_CFG_BANK_EMBED = const(2)
+
+
+
 class LSM6DS3TR():
     """
     A sketchy implementation for talking to an LSM6DS3TR using I2C
     """
 
+    AccelRange = {
+        "RANGE_2G": [0, 2, 0.061],
+        "RANGE_16G": [1, 16, 0.488],
+        "RANGE_4G": [2, 4, 0.122],
+        "RANGE_8G": [3, 8, 0.244],
+    }
+
     def __init__(self, I2C_device, i2c_addr=None):
         self.i2c = I2C_device
         i2c_ids = self.i2c.scan()
         print(f"Found devices {i2c_ids}")
+        self.device_id = i2c_ids[0] # meh...
+        # Setup the range, we use 2G because breaking is rarely above 1G
+        self._cached_accel_range = "RANGE_2G"
+        self.i2c.writeto_mem(self.device_id, 
         return i2c_ids
+
+    def configure_
+
+    def _raw_accel(self):
+        return self.i2c.readfrom_mem(self.device_id, _LSM6DS_OUTX_L_A, 2)
+
+    def acceleration(self) -> Tuple[float, float, float]:
+        x = self._scale_accel(accel[0])
+        y = self._scale_accel(accel[1])
+        z = self._scale_accel(accel[2])
+
+        return (x, y, z)
+
+    def _scale_accel(self, raw_measurement: int) -> float:
+        return (
+            raw_measurement
+            * self.AccelRange[self._cached_accel_range]
+            * _MILLI_G_TO_ACCEL
+        )
