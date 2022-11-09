aa95ce320f2a46e5880f2abd810cc65cc560b513
Holden Karau <holden@pigscanfly.ca>
2022-11-08 21:35:34 -0800

    Get building with circuitpython, which does not yet use manifest files.

--- flash.sh
+++ flash.sh
@@ -3,7 +3,7 @@
 source common.sh
 
 source "${venv_dir}/bin/activate"
-pushd "${build_dir}/microPython/project/micropython/ports/esp32"
+pushd "${build_dir}/circuitpython/project/circuitpython/ports/espressif"
 
 if [ ! -f "${PORT}" ]; then
   PORT=${PORT:-${1:-/dev/ttyUSB0}}
@@ -15,15 +15,22 @@ BOARD=${BOARD:-${2:-"MOTOC3"}}
 BAUD=${BAUD:-115200} #460800} #9600} # 460800
 FLASH_MODE=${FLASH_MODE:-dio} # On low poer device use dout
 
+# Do we need to do an erase?
+
+function erase () {
+  ~/.espressif/python_env/idf4.4_py3.8_env/bin/python \
+    ../../../../../esp-idf/components/esptool_py/esptool/esptool.py
+}
+
 function flash () {
   ~/.espressif/python_env/idf4.4_py3.8_env/bin/python \
     ../../../../../esp-idf/components/esptool_py/esptool/esptool.py \
     ${EXTRA} -p "${PORT}" -b "${BAUD}" --before default_reset \
     --after hard_reset --chip esp32c3  write_flash --flash_mode "${FLASH_MODE}" \
     --flash_size detect --flash_freq 40m 0x0 \
-    "build-${BOARD}/bootloader/bootloader.bin" 0x8000 \
-    "build-${BOARD}/partition_table/partition-table.bin" 0x10000 \
-    "build-${BOARD}/micropython.bin"
+    "build-${BOARD}/esp-idf/bootloader/bootloader.bin" 0x8000 \
+    "build-${BOARD}/esp-idf/partition_table/partition-table.bin" 0x10000 \
+    "build-${BOARD}/circuitpython-firmware.bin"
 }
 
 flash || EXTRA="--no-stub" flash
--- fw/manifest.py
+++ fw/manifest.py
@@ -1,11 +1,4 @@
-include("$(BOARD_DIR)/../manifest.py")
-
-
 freeze(".",
-       ("boot.py",
-        "magici2c.py",
+       ("main.py",
        ),
 )
-package("lsm")
-package("adafruit_bus_device")
-package("adafruit_register")
